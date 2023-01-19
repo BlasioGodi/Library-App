2de7a8ea63190cc28c02b43fbec6008d0ac9a3e1
muhindablasio@gmail.com <muhindablasio@gmail.com>
2023-01-19 19:23:10 +0300

    Js blur effect added

--- script.js
+++ script.js
@@ -6,11 +6,15 @@ function openForm() {
 
 function closeForm() {
     document.getElementById("popup").style.display = "none";
+
+    
+    
 } */
 
 function toggle() {
-    var blur = getElementById('blur');
+    var blur = document.getElementById('blur');
     blur.classList.toggle('active');
-    var popup = getElementById('popup');
+
+    var popup = document.getElementById('popup');
     popup.classList.toggle('active');
 }
