d4b04d9ddb186d8eca2d01d4f373430408831db1
muhindablasio@gmail.com <muhindablasio@gmail.com>
2023-03-17 19:08:35 +0300

    Modified js file

--- script.js
+++ script.js
@@ -1,4 +1,49 @@
+console.log("LIB_APP_INIT");
 
+const BOOK = {
+    title: document.getElementById("bookTitle"),
+    author: document.getElementById("author"),
+    year: document.getElementById("year"),
+    publisher: document.getElementById("publisher"),
+    submit: document.getElementById("book_submit")
+}
+
+let mylibrary = [];
+
+//Add book
+function AddBook() {
+    var book_title = document.querySelector('#bookTitle').value;
+    var book_author = document.querySelector('#author').value;
+    var book_year = document.querySelector('#year').value;
+    var book_publisher = document.querySelector('#publisher').value;
+
+    mylibrary.push(book_title);
+    mylibrary.push(book_author);
+    mylibrary.push(book_year);
+    mylibrary.push(book_publisher);
+
+    mylibrary.forEach(function (book) {
+        console.log(book);
+    });
+}
+
+BOOK.submit.addEventListener('click', AddBook);
+
+function Books(title, author, year, publisher) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.publisher = publisher;
+    this.bookinfo = function () {
+        return title + ',' + author + ',' + year + ',' + publisher;
+    }
+}
+
+const book_one = new Books("Harry Potter", "J.K. Rowlings", "1991", "Kingston");
+
+console.log(book_one.bookinfo());
+
+//Form Popup 
 function toggle() {
     var blur = document.getElementById('blur');
     blur.classList.toggle('active');
@@ -6,13 +51,3 @@ function toggle() {
     var popup = document.getElementById('popup');
     popup.classList.toggle('active');
 }
-
-// let myLibrary = [];
-
-// function Book() {
-//   // the constructor...
-// }
-
-// function addBookToLibrary() {
-//   // do stuff here
-// }
