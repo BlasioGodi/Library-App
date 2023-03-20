651a9e1d704dbc696ee9cd8ac041dad60f5ea3aa
muhindablasio@gmail.com <muhindablasio@gmail.com>
2023-03-20 23:14:33 +0300

    Modified JS

--- script.js
+++ script.js
@@ -29,20 +29,6 @@ function AddBook() {
 
 BOOK.submit.addEventListener('click', AddBook);
 
-function Books(title, author, year, publisher) {
-    this.title = title;
-    this.author = author;
-    this.year = year;
-    this.publisher = publisher;
-    this.bookinfo = function () {
-        return title + ',' + author + ',' + year + ',' + publisher;
-    }
-}
-
-const book_one = new Books("Harry Potter", "J.K. Rowlings", "1991", "Kingston");
-
-console.log(book_one.bookinfo());
-
 //Form Popup 
 function toggle() {
     var blur = document.getElementById('blur');
@@ -51,10 +37,6 @@ function toggle() {
     var popup = document.getElementById('popup');
     popup.classList.toggle('active');
 }
-
-
-
-
 
 
 //BOOK SUPERCLASS
