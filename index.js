7ba40825850ee9498d3be638d49deabab493b978
muhindablasio@gmail.com <muhindablasio@gmail.com>
2023-03-20 22:53:21 +0300

    Testing function prototypes

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-let div = document.querySelector('#blur');
-for (let cssClass of div.classList) {
-    console.log(cssClass);
+//BOOK SUPERCLASS
+function Books() {
 }
+
+//METHOD OF SUPERCLASS
+Books.prototype.BookInfo = function () {
+    console.log(this.title);
+    console.log(this.author);
+    console.log(this.genre);
+}
+
+function Thriller(title, author, genre) {
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+}
+
+Thriller.prototype = Object.create(Books.prototype);
+
+const newBook = new Thriller("Harry Potter and The Chamber of Secrets", "J.K. Rowlings", "Thriller");
+
+//PRINT OUTPUT OF THE FUNCTION PROTOTYPE
+newBook.BookInfo();
+console.log(newBook.title, newBook.author, newBook.genre);
+
+
--- script.js
+++ script.js
@@ -51,3 +51,33 @@ function toggle() {
     var popup = document.getElementById('popup');
     popup.classList.toggle('active');
 }
+
+
+
+
+
+
+//BOOK SUPERCLASS
+function Books() {
+}
+
+//METHOD OF SUPERCLASS
+Books.prototype.BookInfo = function () {
+    console.log(this.title);
+    console.log(this.author);
+    console.log(this.genre);
+}
+
+function Thriller(title, author, genre) {
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+}
+
+Thriller.prototype = Object.create(Books.prototype);
+
+const newBook = new Thriller("Harry Potter and The Chamber of Secrets", "J.K. Rowlings", "Thriller");
+
+//PRINT OUTPUT OF THE FUNCTION PROTOTYPE
+newBook.BookInfo();
+console.log(newBook.title, newBook.author, newBook.genre);
